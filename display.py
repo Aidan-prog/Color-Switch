85a399220c65b7783d7d81ab40fab8be15cd7b97
Viliami Tuanaki <Viliami@users.noreply.github.com>
2016-03-21 15:10:34 +1300

    Update display.py

--- display.py
+++ display.py
@@ -26,8 +26,8 @@ def handle_events():
 			return False
 	return True
 	
-def draw_pixel(x,y, black=True):
-	if(black):
+def draw_pixel(x,y, color=True):
+	if(color):
 		pygame.draw.rect(display, BLACK, (x*SCREEN_SCALE, y*SCREEN_SCALE, SCREEN_SCALE, SCREEN_SCALE))
 	else:
 		pygame.draw.rect(display, WHITE, (x*SCREEN_SCALE, y*SCREEN_SCALE, SCREEN_SCALE, SCREEN_SCALE))
@@ -37,40 +37,46 @@ def sgn(x):
 		return 0
 	return (x / math.fabs(x))
 		
-def draw_line(x0, y0, x1, y1):
+def draw_line(x0, y0, x1, y1, color=True):
 	dX = x1-x0
 	dY = y1-y0
-	error = 0
+	dErr = 0
+	
 	if(dX == 0):
-		dErr = 0
+		for y in range(int(y0), int(y1)):
+			draw_pixel(x0,y, color)
+		return
+	elif(dY == 0):
 		for x in range(int(x0), int(x1)):
-			draw_pixel(x,y0)
-	elif(dy == 0):
-		
+			draw_pixel(x,y0, color)
+		return
 	else:
+		dErr = dY/dX
+		error = 0
 		dErr = math.fabs(dY/dX)
+	print("ran")
 	y = 0
 	for x in range(int(x0), int(x1)):
-		draw_pixel(x,y)
+		draw_pixel(x,y, color)
 		error+=dErr
 		while(error >= 0.5):
-			draw_pixel(x,y)
+			draw_pixel(x,y, color)
 			y+= sgn(dY)
 			error -= 1
 		
-def draw_circle(cX, cY, rad, black=True):
+def draw_circle(cX, cY, rad, color=True):
 	x,y = rad, 0
 	decisionOver2 = 1 - x  # Decision criterion divided by 2 evaluated at x=r, y=0
 
 	while(y <= x):
-		draw_pixel( x + cX,  y + cY) #Octant 1
-		draw_pixel(-x + cX,  y + cY) #Octant 4
-		draw_pixel( y + cX,  x + cY) #Octant 2
-		draw_pixel(-y + cX,  x + cY) #Octant 3
-		draw_pixel( x + cX, -y + cY) #Octant 7
-		draw_pixel(-x + cX, -y + cY) #Octant 5
-		draw_pixel( y + cX, -x + cY) #Octant 8
-		draw_pixel(-y + cX, -x + cY) #Octant 6
+		draw_pixel( x + cX,  y + cY, color) #Octant 1
+		draw_pixel(-x + cX,  y + cY, color) #Octant 4
+		draw_pixel( y + cX,  x + cY, color) #Octant 2
+		draw_pixel(-y + cX,  x + cY, color) #Octant 3
+		draw_pixel( x + cX, -y + cY, color) #Octant 7
+		draw_pixel(-x + cX, -y + cY, color) #Octant 5
+		draw_pixel( y + cX, -x + cY, color) #Octant 8
+		draw_pixel(-y + cX, -x + cY, color) #Octant 6
 		y+=1
 		if (decisionOver2<=0):
 			decisionOver2 += 2 * y + 1   # Change in decision criterion for y -> y+1
@@ -78,20 +84,24 @@ def draw_circle(cX, cY, rad, black=True):
 			x-=1
 			decisionOver2 += 2 * (y - x) + 1   # Change for y -> y+1, x -> x-1
 		
-def draw_filled_circle(cX, cY, rad, black=True):
+def draw_filled_circle(cX, cY, rad, color=True):
 	x, y = rad, 0
 	decisionOver2 = 1 - x 
 	while(y<=x):
-		#draw_pixel( x + cX,  y + cY) #Octant 1
-		#draw_pixel(-x + cX,  y + cY) #Octant 4
 		
-		draw_line(-x+cX, 32, x+cX, 32)
-		#draw_pixel( y + cX,  x + cY) #Octant 2
-		#draw_pixel(-y + cX,  x + cY) #Octant 3
-		draw_pixel( x + cX, -y + cY) #Octant 7
-		draw_pixel(-x + cX, -y + cY) #Octant 5
-		draw_pixel( y + cX, -x + cY) #Octant 8
-		draw_pixel(-y + cX, -x + cY) #Octant 6
+		draw_line(-x+cX, y+cY, x+cX, y+cY, color)
+		draw_line(-y+cX, -x+cY, y+cX, -x+cY, color)
+		draw_line(-x+cX, -y+cY, x+cX, -y+cY, color)
+		draw_line(-y+cX, x+cY, y+cX, x+cY, color)
+		
+		draw_pixel( x + cX,  y + cY, color) #Octant 1
+		draw_pixel(-x + cX,  y + cY, color) #Octant 4
+		draw_pixel( y + cX,  x + cY, color) #Octant 2
+		draw_pixel(-y + cX,  x + cY, color) #Octant 3
+		draw_pixel( x + cX, -y + cY, color) #Octant 7
+		draw_pixel(-x + cX, -y + cY, color) #Octant 5
+		draw_pixel( y + cX, -x + cY, color) #Octant 8
+		draw_pixel(-y + cX, -x + cY, color) #Octant 6
 		y+=1
 		if (decisionOver2<=0):
 			decisionOver2 += 2 * y + 1   # Change in decision criterion for y -> y+1
@@ -102,10 +112,11 @@ def draw_filled_circle(cX, cY, rad, black=True):
 		
 while(handle_events()):
 	clock.tick(60)
-	display.fill(WHITE)
+	display.fill(BLACK)
 	
-	draw_filled_circle(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, radius)
-	draw_line(0,64, 128, 64)
+	draw_filled_circle(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, radius, False)
+	#draw_line(0,63, 128, 63)
+	#draw_pixel(123,63)
 	
 	pygame.display.flip()
 
